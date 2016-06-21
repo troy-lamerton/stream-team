3fc91db6a0dcf51729ac2e5918386e5301541a94
Troy Lamerton <troy.lamerton@gmail.com>
2016-06-22 11:18:05 +1200

    Configure handlebars to compile display page

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express')
+var fs = require('fs')
+var Handlebars = require('handlebars')
 var app = express()
 var PORT = process.env.PORT || 3000
 
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
-	res.send('<body>Hello, this is where the image will go</body>')
+  var source = fs.readFileSync('views/display.hbs')
+  var template =  Handlebars.compile(source.toString())
+
+  res.send(template({imageUrl: 'http://handlebarsjs.com/images/handlebars_logo.png'}))
+
+	// res.render('display', {img: "www.google.com"})
 })
 
 app.listen(PORT)
+console.log('Server is listening on http://localhost:' + PORT)
