e57723c975223495174ff579d92353a126af73bf
Troy Lamerton <troy.lamerton@gmail.com>
2016-07-18 15:21:09 +1200

    Update development database config

--- knexfile.js
+++ knexfile.js
@@ -34,20 +34,6 @@ module.exports = {
     }
   },
 
-  production: {
-    client: process.env.DATABASE_URL || 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  production: process.env.DATABASE_URL
 
 };
