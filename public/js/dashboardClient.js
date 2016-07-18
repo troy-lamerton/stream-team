bea77875d6c209a23fb6d9f3c23484e4f35326a8
Troy Lamerton <troy.lamerton@gmail.com>
2016-07-18 15:51:46 +1200

    Remove hardcoded locahost reference and unused keyboard shortcuts

--- public/js/dashboardClient.js
+++ public/js/dashboardClient.js
@@ -1,16 +1,5 @@
 // dashboard client
-var serverUrl = "http://localhost:3000"
-var socket = io.connect(serverUrl + "/dashboard")
-
-window.onkeyup = function (e) {
-  // keys 1 to 9, 1 has the keyCode 49.
-  if(e.keyCode >= 49 && e.keyCode <= 57) {
-    var sceneIndex = e.keyCode - 49
-    // pressing 1 gets the first image in the array
-    console.log('setting new scene:', sceneIndex)
-    socket.emit('set scene', sceneIndex)
-  }
-}
+var socket = io.connect("/dashboard")
 
 function newScene (sceneIndex) {
   console.log('setting new scene: ', sceneIndex)
